beb10973b2ffd8e69bbf7e44196ba3d37a6a6a02
Hello my name is Akmal <108610205+StorySad@users.noreply.github.com>
2022-10-24 22:55:04 +0700

    Update main-sewa.js

--- plugins/main-sewa.js
+++ plugins/main-sewa.js
@@ -9,15 +9,15 @@ let handler = async (m, { conn }) => {
 ┌「 *Sewa bot* 」
 │     
 ├ 1 Grup / 30 Hari
-├ Rp. 10,000 Gopay
-├ Rp. 15,000 Pulsa Three
+├ Rp. 10,000 Dana
+├ Rp. 15,000 Pulsa Telkom
 │
 ├ 1 Premium / 30 Hari
-├ Rp. 5,000 Gopay
-├ Rp. 10,000 Pulsa Three
+├ Rp. 5,000 Dana
+├ Rp. 10,000 Pulsa Telkom
 │
 ├ tertarik? hubungi: 
-├ @62895336282144 (The.sad.boy01)
+├ @6281283186765 (The.sad.boy01)
 └────
 `.trim(),
         "listType": "PRODUCT_LIST",
@@ -38,7 +38,7 @@ let handler = async (m, { conn }) => {
             },
             "businessOwnerJid": "6288217277973@s.whatsapp.net"
         },
-        "footerText": "https://wa.me/c/6288217277973\n\nowner number : wa.me/62895336282144"
+        "footerText": "https://wa.me/c/6281283186765\n\nowner number : wa.me/6281283186765"
     }
     conn.sendMessage(m.chat, listMessage, 'listMessage', { quoted: m, contextInfo: { mentionedJid: conn.parseMention(listMessage.description) } })
 }
